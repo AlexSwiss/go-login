03ff03b80bd5058d7a82a9282c731009e65692f4
AlexSwiss <alexyikeh@gmail.com>
2020-07-18 20:32:32 +0100

    added database connection

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/AlexSwiss/al_ik
 go 1.14
 
 require (
-	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/go-sql-driver/mysql v1.5.0
 	github.com/kataras/go-sessions v3.0.0+incompatible // indirect
 	github.com/satori/go.uuid v1.2.0 // indirect
 	github.com/valyala/fasthttp v1.15.1 // indirect
--- server.go
+++ server.go
@@ -1,5 +1,40 @@
-package server
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+var err error
+
+func connectDB() {
+	db, err = sql.Open("mysql", "root:@kingkunta@/alik_db")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func routes() {
+
+}
 
 func main() {
+	connectDB()
+	routes()
 
+	defer db.Close()
+
+	fmt.Println("Server running on port :8080")
+	http.ListenAndServe(":8080", nil)
 }
